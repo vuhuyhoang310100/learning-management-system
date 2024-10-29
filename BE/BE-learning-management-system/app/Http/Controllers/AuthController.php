3f2ebd959cb69c070b3310313ac656ecf0f81d28
Dev <dev@appifai.com>
2024-10-29 10:39:57 +0700

    auth/complete

---
+++ BE/BE-learning-management-system/app/Http/Controllers/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use App\Traits\HttpResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    //
+    use HttpResponse;
+
+    public function login(LoginUserRequest $request)
+{
+    $request->validated($request->all());
+
+    if (!Auth::attempt($request->only('email', 'password'))) {
+        return $this->error('', 'Invalid credentials', 401);
+    }
+        $user = Auth::user();
+        $token = $user->createToken('API TOKEN of ' . $user->name)->plainTextToken;
+
+        return $this->success([
+            'user' => $user,
+            'message' => 'Login successful',
+            'token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+
+}
+
+    public function register(StoreUserRequest $request)
+    {
+        $request->validated($request->all());
+        $user = User::firstOrCreate([
+            'name' => $request->name,
+            'email'=>$request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        return $this->success(
+            ['user'=>$user,
+            'token'=>$user->createToken('API TOKEN of '.$user->name)->plainTextToken
+            ]
+        );
+    }
+    public function logout()
+    {
+        $user = Auth::user();
+
+        $user->currentAccessToken()->delete();
+
+        return $this->success([
+            'message'=>'Logged out successfully',
+        ]);
+    }
+
+}
--- BE/BE-learning-management-system/routes/api.php
+++ BE/BE-learning-management-system/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//Public Route
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login'])->name('auth-login');
+    Route::post('/register', [AuthController::class, 'register'])->name('auth-register');
+});
+//protected Route
+Route::group(['middleware' => ['auth.user']], function(){
+    Route::post('/logout',[AuthController::class,'logout'])->name('auth-logout');
 });
